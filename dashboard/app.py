7792dc2a31180519f2578b44a95138e0b01a0fcb
Steve Federowicz <steve@veliatx.com>
2023-06-15 04:42:01 +0000

    working on table UI

--- dashboard/app.py
+++ dashboard/app.py
@@ -20,6 +20,13 @@ from streamlit_echarts import st_echarts
 from scipy.cluster.hierarchy import linkage, leaves_list
 
 
+from pandas.api.types import (
+    is_categorical_dtype,
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+    is_object_dtype,
+)
+
 from plotting import plot_structure_plddt
 
 # Load data
@@ -160,21 +167,97 @@ def load_tcga_tumor_vs_nat(xena_metadata, xena_expression):
     return ave_per_transcript_per_cancer
 
 
+
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+
+    df = df.copy()
+
+    # Try to convert datetimes into a standard format (datetime, no timezone)
+    for col in df.columns:
+        if is_object_dtype(df[col]):
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except Exception:
+                pass
+
+        if is_datetime64_any_dtype(df[col]):
+            df[col] = df[col].dt.tz_localize(None)
+
+    modification_container = st.container()
+
+    with modification_container:
+        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
+        for column in to_filter_columns:
+            left, right = st.columns((1, 20))
+            # Treat columns with < 10 unique values as categorical
+            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_numeric_dtype(df[column]):
+                _min = float(df[column].min())
+                _max = float(df[column].max())
+                step = (_max - _min) / 100
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=step,
+                )
+                df = df[df[column].between(*user_num_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
+
+
 def sorf_table(sorf_excel_table):
     st.title("sORF Library")
     st.write("Table contains secreted sORFs.")
-    gd = GridOptionsBuilder.from_dataframe(sorf_excel_table)
-    gd.configure_pagination(enabled=True)
-    gd.configure_selection(use_checkbox=True)
-    gridOptions = gd.build()
-    ag_grid = AgGrid(sorf_excel_table, gridOptions = gridOptions,
-                     # fit_columns_on_grid_load = True,
-                     height=500,
-                     width = "200%",
-                     theme = "streamlit",
-                     update_mode = GridUpdateMode.GRID_CHANGED,
-                     reload_data = False,
-                     editable = False)
+    df = filter_dataframe(sorf_excel_table)
+    st.dataframe(df)
+    st.download_button(
+        label="Download data as CSV",
+        data=df,
+        file_name='large_df.csv',
+        mime='text/csv',
+    )
 
 
 def sorf_heatmap():
