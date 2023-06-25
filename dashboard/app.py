8b5a6ad7a04c8023bd4352f7c4ebebefd45fbc51
Steve Federowicz <steve@veliatx.com>
2023-06-25 22:07:57 +0000

    adding row select from table for detail viewing

--- dashboard/app.py
+++ dashboard/app.py
@@ -19,23 +19,23 @@ from statsmodels.stats.multitest import multipletests
 from streamlit_echarts import st_echarts
 from scipy.cluster.hierarchy import linkage, leaves_list
 
-
-from pandas.api.types import (
-    is_categorical_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-    is_object_dtype,
-)
-
+from dashboard.util import filter_dataframe
 from plotting import plot_structure_plddt
+
 
 # Load data
 @st.cache_data()
 def load_sorf_excel():
     """
     """
-    sorf_excel_table = pd.read_excel('../data/interim_phase1to6_secreted_hits_20230330.xlsx')
-    return sorf_excel_table
+    sorf_excel_df = pd.read_excel('../data/interim_phase1to6_secreted_hits_20230330.xlsx')
+    sorf_excel_df['show_details'] = False
+
+    cols = list(sorf_excel_df.columns)
+    cols.insert(0, cols.pop(cols.index('show_details')))
+
+    sorf_excel_df = sorf_excel_df[cols]
+    return sorf_excel_df
 
 
 @st.cache_data()
@@ -58,6 +58,7 @@ def load_xena_tcga_gtex_target():
     vtx_id_to_transcripts = json.load(open('../data/vtx_to_ensembl_ids.json', 'r'))
     return xena_metadata, xena_expression, vtx_id_to_transcripts
 
+
 @st.cache_data()
 def load_xena_heatmap():
     
@@ -75,6 +76,11 @@ def load_xena_heatmap():
     xena_exp_df = xena_exp_df.merge(xena_metadata_df, left_index=True, right_index=True)
     transcript_col_names = [i for i in xena_exp_df.columns if i not in xena_metadata_df.columns]
     xena_exp_df = xena_exp_df[transcript_col_names].groupby(xena_exp_df['_primary_site']).aggregate(np.mean)
+
+    threshold = .5
+    mean_vals = xena_exp_df.max()
+    cols_to_remove = mean_vals[mean_vals < threshold].index
+    xena_exp_df = xena_exp_df.drop(cols_to_remove, axis=1)
 
     # compute the clusters
     row_clusters = linkage(xena_exp_df.T.values, method='complete', metric='euclidean')
@@ -95,6 +101,7 @@ def load_xena_heatmap():
     row_names = list(row_map.keys())
 
     return data, col_names, row_names
+
 
 @st.cache_data()
 def load_mouse_blastp_results():
@@ -167,97 +174,51 @@ def load_tcga_tumor_vs_nat(xena_metadata, xena_expression):
     return ave_per_transcript_per_cancer
 
 
-
-@st.cache
+@st.cache_data
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
 
+def sorf_table(sorf_excel_df):
+    st.title('sORF Library')
+    st.write('Table contains secreted sORFs.')
+    df = filter_dataframe(sorf_excel_df)
 
-def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Adds a UI on top of a dataframe to let viewers filter columns
+    if 'data_editor_prev' in st.session_state.keys():
+        curr_rows = st.session_state['data_editor']['edited_rows']
+        prev_rows = st.session_state['data_editor_prev']['edited_rows']
 
-    Args:
-        df (pd.DataFrame): Original dataframe
+        update_rows = {key: curr_rows[key] for key in curr_rows if key not in prev_rows}
 
-    Returns:
-        pd.DataFrame: Filtered dataframe
-    """
+        row_idx = list(update_rows.keys())[0]
+        df['show_details'] = False
+        df.at[row_idx, 'show_details'] = True
+        st.session_state['curr_vtx_id'] = str(df.loc[row_idx]['vtx_id'])
 
-    df = df.copy()
+    update_df = st.data_editor(
+        df,
+        column_config={
+            'vtx_id': st.column_config.TextColumn(disabled=True),
+        },
+        key='data_editor'
+    )
 
-    # Try to convert datetimes into a standard format (datetime, no timezone)
-    for col in df.columns:
-        if is_object_dtype(df[col]):
-            try:
-                df[col] = pd.to_datetime(df[col])
-            except Exception:
-                pass
+    st.session_state['data_editor_prev'] = st.session_state['data_editor'].copy()
 
-        if is_datetime64_any_dtype(df[col]):
-            df[col] = df[col].dt.tz_localize(None)
+    if 'curr_vtx_id' in st.session_state.keys():
+        current_row = df[df['vtx_id'] == st.session_state['curr_vtx_id']]
+        st.write(current_row)
+        print('jmmmmm')
 
-    modification_container = st.container()
-
-    with modification_container:
-        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
-        for column in to_filter_columns:
-            left, right = st.columns((1, 20))
-            # Treat columns with < 10 unique values as categorical
-            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                user_cat_input = right.multiselect(
-                    f"Values for {column}",
-                    df[column].unique(),
-                    default=list(df[column].unique()),
-                )
-                df = df[df[column].isin(user_cat_input)]
-            elif is_numeric_dtype(df[column]):
-                _min = float(df[column].min())
-                _max = float(df[column].max())
-                step = (_max - _min) / 100
-                user_num_input = right.slider(
-                    f"Values for {column}",
-                    min_value=_min,
-                    max_value=_max,
-                    value=(_min, _max),
-                    step=step,
-                )
-                df = df[df[column].between(*user_num_input)]
-            elif is_datetime64_any_dtype(df[column]):
-                user_date_input = right.date_input(
-                    f"Values for {column}",
-                    value=(
-                        df[column].min(),
-                        df[column].max(),
-                    ),
-                )
-                if len(user_date_input) == 2:
-                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                    start_date, end_date = user_date_input
-                    df = df.loc[df[column].between(start_date, end_date)]
-            else:
-                user_text_input = right.text_input(
-                    f"Substring or regex in {column}",
-                )
-                if user_text_input:
-                    df = df[df[column].astype(str).str.contains(user_text_input)]
-
-    return df
-
-
-def sorf_table(sorf_excel_table):
-    st.title("sORF Library")
-    st.write("Table contains secreted sORFs.")
-    df = filter_dataframe(sorf_excel_table)
-    st.dataframe(df)
     st.download_button(
         label="Download data as CSV",
-        data=df,
-        file_name='large_df.csv',
+        data=convert_df(update_df),
+        file_name='sorf_selection.csv',
         mime='text/csv',
     )
+
+
 
 
 def sorf_heatmap():
@@ -311,7 +272,6 @@ def sorf_heatmap():
         st_echarts(option, height="1000px")
 
 
-
 def ucsc_link(chrom, start, end):
     base_link = "http://genome.ucsc.edu/cgi-bin/hgTracks?"
     genome = "db=hg38"
@@ -320,18 +280,17 @@ def ucsc_link(chrom, start, end):
     return ucsc_link
 
 
-# Function per page
 def details(sorf_excel_table, xena_expression, xena_metadata, vtx_id_to_transcripts, ave_per_transcript_per_cancer, esmfold, blastp_mouse_hits):
     # Header text
     st.title('sORF Details')
     # Select a single sORF to plot and convert ID to vtx_id
     st.session_state['id_type_selected'] = st.radio("ID Type", options=('vtx_id', 'primary_id', 'genscript_id'), index=0, horizontal=True)
-    st.session_state['detail_id_seleceted'] = st.selectbox("Select ORF", options = sorf_excel_table[st.session_state['id_type_selected']])
+    st.session_state['detail_id_selected'] = st.selectbox("Select ORF", options = sorf_excel_table[st.session_state['id_type_selected']])
     if st.session_state['id_type_selected'] == 'vtx_id':
-        vtx_id = st.session_state['detail_id_seleceted']
+        vtx_id = st.session_state['detail_id_selected']
     else:
-        vtx_id = sorf_excel_table[sorf_excel_table[st.session_state['id_type_selected']] == st.session_state['detail_id_seleceted']].iloc[0]['vtx_id']
-    selected_row = sorf_excel_table[sorf_excel_table[st.session_state['id_type_selected']] == st.session_state['detail_id_seleceted']].iloc[0]
+        vtx_id = sorf_excel_table[sorf_excel_table[st.session_state['id_type_selected']] == st.session_state['detail_id_selected']].iloc[0]['vtx_id']
+    selected_row = sorf_excel_table[sorf_excel_table[st.session_state['id_type_selected']] == st.session_state['detail_id_selected']].iloc[0]
     link = ucsc_link(selected_row['chromosome'], selected_row['start'], selected_row['end'])
     st.write(f"UCSC Browser [link]({link})")
 
@@ -373,9 +332,9 @@ def details(sorf_excel_table, xena_expression, xena_metadata, vtx_id_to_transcri
     st.pyplot(fig)
     # Blastp Mouse
     if st.session_state['id_type_selected'] == 'primary_id':
-        primary_id = st.session_state['detail_id_seleceted']
+        primary_id = st.session_state['detail_id_selected']
     else:
-        primary_id = sorf_excel_table[sorf_excel_table[st.session_state['id_type_selected']] == st.session_state['detail_id_seleceted']].iloc[0]['primary_id']
+        primary_id = sorf_excel_table[sorf_excel_table[st.session_state['id_type_selected']] == st.session_state['detail_id_selected']].iloc[0]['primary_id']
     blastp_results_selected_sorf = blastp_mouse_hits[primary_id]
     if len(blastp_results_selected_sorf) == 0:
         long_text = "No alignments with mouse found." #st.write('No alignments with mouse found.')
@@ -399,7 +358,6 @@ def selector(sorf_excel_table):
     st.write(selected_points)
 
 
-# Define the main function that will run the app
 def main():
     #st.sidebar.title("Navigation")
     
@@ -414,7 +372,7 @@ def main():
         "sORF Selector": selector
     }
     
-    sorf_excel_table = load_sorf_excel()
+    sorf_excel_df = load_sorf_excel()
     xena_metadata, xena_expression, vtx_id_to_transcripts = load_xena_tcga_gtex_target()
     esmfold = load_esmfold()
     blastp_mouse_hits = load_mouse_blastp_results()
@@ -423,18 +381,18 @@ def main():
     tab1, tab2, tab3, tab4 = st.tabs(list(pages.keys()))
 
     with tab1:
-        sorf_table(sorf_excel_table)
+        sorf_table(sorf_excel_df)
 
     with tab2:
         sorf_heatmap()
     
     with tab3:
-        details(sorf_excel_table, xena_expression, xena_metadata, vtx_id_to_transcripts, ave_per_transcript_per_cancer, esmfold, blastp_mouse_hits)
+        details(sorf_excel_df, xena_expression, xena_metadata, vtx_id_to_transcripts, ave_per_transcript_per_cancer, esmfold, blastp_mouse_hits)
         
     with tab4:
-        selector(sorf_excel_table)
+        selector(sorf_excel_df)
 
-# Run the app
+
 if __name__ == "__main__":
 
     main()
